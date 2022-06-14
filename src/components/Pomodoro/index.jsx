cf7bbdb5798282c4bac08bd203adc106ffb16bb5
MassimaMao <mastem@msn.com>
2022-06-14 06:23:16 +0200

    working countdown

--- src/components/Pomodoro/index.jsx
+++ src/components/Pomodoro/index.jsx
@@ -5,7 +5,29 @@ function Pomodoro() {
   const [seconds, setSeconds] = useState(0);
   const [displayMessage, setDisplayMessage] = useState(false);
 
-  useEffect(() => {}), [seconds];
+  useEffect(() => {
+    let interval = setInterval(() => {
+      clearInterval(interval);
+
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          let minutes = displayMessage ? 24 : 4;
+          let seconds = 59;
+
+          setSeconds(seconds);
+          setMinutes(minutes);
+          setDisplayMessage(!displayMessage);
+          //
+        }
+      } else {
+        setSeconds(seconds - 1);
+      }
+    }, 1000);
+  }, [seconds]);
+
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
   return (
